789242b55e52300cad7f7431e980935372474ed3
SKIPAH <juzzii.mehta@gmail.com>
2025-01-05 22:23:44 +0200

    mini progress

--- RANDOMTESTS/test.js
+++ RANDOMTESTS/test.js
@@ -1,3 +1,7 @@
+//BINARY TREE STUFF
+
+
+/*
 class Node {
     constructor(data) {
         this.data = data;  // The value of the node
@@ -47,3 +51,46 @@ tree.insert(3);
 tree.insert(7);
 
 console.log(JSON.stringify(tree, null, 2)); // Print the tree structure
+*/
+
+/*
+
+Given an integer n, for every positive integer i <= n, the task is to print,
+
+    “FizzBuzz” if i is divisible by 3 and 5,
+    “Fizz” if i is divisible by 3,
+    “Buzz” if i is divisible by 5
+    “i” as a string, if none of the conditions are true.
+    */
+
+
+
+
+
+
+    //fizzbuzz
+
+    /*
+    function fizzBuzz(n){
+        let result = [];
+        for(let i = 1; i <= n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+        result.push("FizzBuzz");
+        }
+        else if(i % 3 === 0){
+        result.push("Fizz");
+        }
+        else if(i % 5 === 0){
+        result.push("Buzz");
+        }
+        else {
+        result.push(i.toString());
+        }
+        }
+        return result;
+    }
+
+    let n = 20;
+    let result = fizzBuzz(n);
+    console.log(result.join(' '));
+    */
